547bad95b9bea921a6aa07c0430cf1cce648181d
Namanyay Goel <mail@namanyayg.com>
2015-01-18 17:11:48 +0530

    Completing testing order-by-date

--- tests/rest_spec.js
+++ tests/rest_spec.js
@@ -4,29 +4,59 @@ var frisby = require('frisby');
 // Generic testing functions
 //
 function testDebug (d) {
-  expect(d.count).toEqual(jasmine.any(Number));
-  expect(d.request.request).toEqual(jasmine.any(String));
-  expect(d.request['interface']).toEqual(jasmine.any(String));
-  expect(d.request.referer).toEqual(jasmine.any(String));
-  expect(d.request.auth).toEqual(jasmine.any(String));
-  expect(d.request.type).toEqual(jasmine.any(String));
-  expect(d.request['__internal']).toBeDefined();
-  expect(d.request.host).toEqual(jasmine.any(String));
-  expect(d['type-handler']).toBeDefined();
+  expect( d.count ).toEqual( jasmine.any(Number) );
+  expect( d.request.request ).toEqual( jasmine.any(String) );
+  expect( d.request['interface'] ).toEqual( jasmine.any(String) );
+  expect( d.request.referer ).toEqual( jasmine.any(String) );
+  expect( d.request.auth ).toEqual( jasmine.any(String) );
+  expect( d.request.type ).toEqual( jasmine.any(String) );
+  expect( d.request['__internal'] ).toBeDefined();
+  expect( d.request.host ).toEqual( jasmine.any(String) );
+  expect( d['type-handler'] ).toBeDefined();
 }
 
 function testMe (me) {
-  expect(me.name).toEqual(jasmine.any(String));
-  expect(me.documents).toEqual(jasmine.any(Number));
-  expect(me.id).toEqual(jasmine.any(String));
-  expect(me.path).toEqual(jasmine.any(String));
-  expect(me.description).toEqual(jasmine.any(String));
-  expect(me.vsession).toEqual(jasmine.any(String));
-  expect(me['class']).toEqual(jasmine.any(String));
-  expect(me.oid).toEqual(jasmine.any(Number));
-  expect(me.links).toEqual(jasmine.any(Number));
-  expect(me.icon).toBeDefined();
-  expect(me.fullname).toEqual(jasmine.any(String));
+  expect( me.name ).toEqual( jasmine.any(String) );
+  expect( me.documents ).toEqual( jasmine.any(Number) );
+  expect( me.id ).toEqual( jasmine.any(String) );
+  expect( me.path ).toEqual( jasmine.any(String) );
+  expect( me.description ).toEqual( jasmine.any(String) );
+  expect( me.vsession ).toEqual( jasmine.any(String) );
+  expect( me['class'] ).toEqual( jasmine.any(String) );
+  expect( me.oid ).toEqual( jasmine.any(Number) );
+  expect( me.links ).toEqual( jasmine.any(Number) );
+  expect( me.icon ).toBeDefined();
+  expect( me.fullname ).toEqual( jasmine.any(String) );
+}
+
+function testEvent(e) {
+  expect(e['class']).toEqual( jasmine.any(String) );
+  expect(e.title).toEqual( jasmine.any(String) );
+  expect(e.name).toEqual( jasmine.any(String) );
+  expect(e.id).toEqual( jasmine.any(String) );
+  expect(e.path).toEqual( jasmine.any(String) );
+  expect(e.type).toEqual( jasmine.any(String) );
+
+  expect(e.oid).toEqual( jasmine.any(Number) );
+
+  toBeStringIfExists( e.description );
+  toBeStringIfExists( e.eventid );
+  toBeStringIfExists( e.category );
+  toBeStringIfExists( e.owner );
+  toBeStringIfExists( e.address );
+  toBeStringIfExists( e.city );
+  toBeStringIfExists( e['time'] );
+
+  toBeDateIfExists( e.date );
+
+  toBeObjectIfExists( e.events );
+  toBeObjectIfExists( e.schedule );
+  
+  if ( e.keywords ) {
+    e.keywords.forEach(function(word) {
+      expect( word ).toEqual( jasmine.any(String) );
+    })
+  };
 }
 
 
@@ -35,11 +65,17 @@ function testMe (me) {
 //
 function toBeStringIfExists(val) {
   if ( val ) 
-    expect(val).toEqual(jasmine.any(String));
+    expect( val ).toEqual( jasmine.any(String) );
 }
 function toBeObjectIfExists(val) {
   if ( val ) 
-    expect(val).toEqual(jasmine.any(Object));
+    expect( val ).toEqual( jasmine.any(Object) );
+}
+function toBeDateIfExists (val) {
+  if ( val ) {
+    var date = new Date(val);
+    expect( date ).toEqual( jasmine.any(Object) );
+  }
 }
 
 
@@ -54,27 +90,27 @@ frisby.create('Test techgrind.events to be well-formed')
     "request-method": "GET",
     "debug": testDebug,
     "me": testMe,
+    "event-list": function(val) { 
+      val.forEach(function(e) {
+        testEvent(e);
+      });
+    }
   })
-  .expectJSONTypes('event-list.*', {
-    "class": String,
-    "title": String,
-    "oid": Number,
-    "name": String,
-    "id": String,
-    "description": toBeStringIfExists,
-    "category": toBeStringIfExists,
-    "path": String,
-    "eventid": toBeStringIfExists,
-    "type": String,
-    "owner": toBeStringIfExists,
-    "events": toBeObjectIfExists,
-    "keywords": function(kw) {
-      if ( kw ) {
-        kw.forEach(function(word) {
-          expect(word).toEqual(jasmine.any(String));
-        })
-      }
-    },
-    "schedule": toBeObjectIfExists
+  .toss();
+
+
+frisby.create('Test techgrind.events/order-by-date to be well-formed')
+  .get('http://dev-back1.techgrind.asia/scripts/rest.pike?request=techgrind.events/order-by-date')
+  .expectStatus(200)
+  .expectJSON({
+    "request": "techgrind.events/order-by-date",
+    "request-method": "GET",
+    "debug": testDebug,
+    "me": testMe,
+    "event-list": function(val) { 
+      val.forEach(function(e) {
+        testEvent(e);
+      });
+    }
   })
   .toss();
